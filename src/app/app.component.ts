54dc55f350cbb8ef1969432a643b25a7e75b5a66
cenkAkay <cenkakay@rotateknik.com.tr>
2021-10-05 12:09:03 +0300

    assignment finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  formProject: FormGroup;
+  projectStatus = ["Stable", "Critical", "Finished"];
+  ngOnInit() {
+    this.formProject = new FormGroup({
+      projectname: new FormControl(null, [
+        Validators.required,
+        this.forbiddenNames.bind(this),
+      ]),
+      email: new FormControl(
+        null,
+        [Validators.required, Validators.email],
+        this.forbiddenEmails.bind(this)
+      ),
+      projectStatus: new FormControl(null),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formProject.status);
+  }
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (control.value === "Test") {
+      return {
+        nameIsForbidden: true,
+      };
+    } else {
+      return null;
+    }
+  }
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === "test@test") {
+          resolve({
+            emailForbidden: true,
+          });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
 }
